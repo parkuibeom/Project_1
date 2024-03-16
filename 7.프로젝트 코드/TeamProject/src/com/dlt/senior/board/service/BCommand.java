8d75c7d08130637ac8777ea6a5af1b2f282f6b5e
parkuibeom <dmlqja011@naver.com>
2024-03-16 22:04:46 +0900

    no message

--- 7.프로젝트 코드/TeamProject/src/com/dlt/senior/board/service/BCommand.java
+++
@@ -1,8 +0,0 @@
-package com.dlt.senior.board.service;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-public interface BCommand {
-	public void execute(HttpServletRequest request, HttpServletResponse response);
-}
--- 7.프로젝트 코드/TeamProject/src/com/dlt/senior/userMgmt/service/UCommand.java
+++
@@ -1,8 +0,0 @@
-package com.dlt.senior.userMgmt.service;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-public interface UCommand {
-	public void execute(HttpServletRequest request, HttpServletResponse response);
-}
